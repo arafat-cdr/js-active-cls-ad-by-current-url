ee31aa92a6e953b1773dfba1983338fa817ce6b8
Arafat <arafat.dml@gmail.com>
2020-03-29 00:03:47 +0600

    Update and rename index.js to index.php

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<li class="nav-parent">
+    <a class="nav-link" href="#">
+        <i class="fas fa-columns" aria-hidden="true"></i>
+        <span>Users Module</span>
+    </a>
+    <ul class="nav nav-children">
+        <li class="">
+            <a class="nav-link" href="<?php route("user-create"); ?>">
+                Create User
+            </a>
+        </li>
+        <li class="">
+            <a class="nav-link" href="<?php route("user-list"); ?>">
+                List User
+            </a>
+        </li>
+    </ul>
+</li>
+
+
+
+<script>
+
 /***************************************************
 || This Will add the nav-active Class
 || On the Parent of Module Name li class
@@ -14,3 +37,5 @@
 $('.nav-link').filter(function(){
 return window.location.href.includes($(this).attr('href'));
 }).parents().addClass('nav-active').parents().addClass("nav-active nav-expanded");
+                  
+</script>
